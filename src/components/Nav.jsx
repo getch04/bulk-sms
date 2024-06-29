77879f44cf557dbd3727ba128b5be398d011fb60
get_ <39654442+getch04@users.noreply.github.com>
2024-06-29 13:27:11 +0300

    feat: implement the menu visibility

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,28 @@
-import { hamburger } from "../assets/icons";
+import React, { useState } from "react";
+import { close, hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
-
 import { navLinks } from "../constants";
 
 const Nav = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const handleMenuItemClick = () => {
+    setMenuVisible(false);
+  };
+
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
+    <header className="padding-x py-8 absolute z-[11] w-full">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img src={headerLogo} alt="logo" width={130} height={29} />
         </a>
-        <ul className="flex-1 flex  justify-center items-center gap-16 max-lg:hidden">
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item, index) => (
-            <li key={item.label}>
+            <li key={`${item.label}-${index}`}>
               <a
                 href={item.href}
                 className="font-montserrat text-sm text-gray-500 hover:text-gray-900"
@@ -23,9 +33,43 @@ const Nav = () => {
           ))}
         </ul>
         <div className="hidden max-lg:block">
-          <img src={hamburger} alt="hamburger" width={25} height={25} />
+          <img
+            src={hamburger}
+            alt="hamburger"
+            width={25}
+            height={25}
+            onClick={handleMenuToggle}
+          />
         </div>
       </nav>
+      <div
+        className={`fixed top-0 right-0 w-[300px] h-full bg-white transition-transform duration-300 transform ${
+          menuVisible ? "translate-x-0" : "translate-x-full"
+        } z-50`}
+      >
+        <div className="flex justify-end m-10">
+          <img
+            src={close}
+            alt="close"
+            width={25}
+            height={25}
+            onClick={handleMenuToggle}
+          />
+        </div>
+        <ul className="flex flex-col items-center justify-center h-full gap-4">
+          {navLinks.map((item, index) => (
+            <li key={`${item.label}-${index}`} className="w-full text-center">
+              <a
+                href={item.href}
+                className="font-montserrat text-lg text-gray-500 hover:text-gray-900 w-full block"
+                onClick={handleMenuItemClick}
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
   );
 };
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,69 @@
+import { copyrightSign } from "../assets/icons";
+import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="footer logo" width={150} height={46} />
+          </a>
+          <p className="leading-7 mt-6 text-base font-montserrat text-white-400 sm:max-w-sm">
+            Get shoes ready for any occasion with our collection of shoes.Find
+            your perfect size in the store.
+          </p>
+          <div className="flex items-center mt-8 gap-5">
+            {socialMedia.map((item, index) => (
+              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
+                <img
+                  key={index}
+                  src={item.src}
+                  alt={item.alt}
+                  width={24}
+                  height={24}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+          {footerLinks.map((section, index) => (
+            <div key={section}>
+              <h4 className="text-white font-montserrat font-bold text-lg">
+                {section.title}
+              </h4>
+              <div className="flex flex-col gap-2 mt-5">
+                {section.links.map((link, index) => (
+                  <a
+                    key={index}
+                    href={link.link}
+                    className="text-white-400 font-montserrat text-base"
+                  >
+                    {link.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm: items-center">
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <img
+            src={copyrightSign}
+            alt="Copyright"
+            width={20}
+            height={20}
+            className="rounded-full m-0"
+          />
+          <p>Copyright. All rights reserved</p>
+        </div>
+        <p className="cursor-pointer font-montserrat">Terms & conditions</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
